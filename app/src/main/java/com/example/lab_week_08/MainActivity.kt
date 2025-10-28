b246fb2c9bef8719e7f2c4924027e5e89d58bd07
ncholasandre <andrenick1208@gmail.com>
2025-10-28 14:50:17 +0700

    Commit No. 1 – add first & second worker

--- app/src/main/java/com/example/lab_week_08/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.lab_week_08
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.map_mid_term.worker.FirstWorker
+import com.example.map_mid_term.worker.SecondWorker
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnStart = findViewById<Button>(R.id.btnStartWork)
+
+        btnStart.setOnClickListener {
+            startWorkChain()
         }
+    }
+
+    private fun startWorkChain() {
+        // Kirim data awal
+        val inputData = Data.Builder()
+            .putString(FirstWorker.INPUT_DATA_ID, "Job_001")
+            .build()
+
+        // Worker pertama
+        val firstWork = OneTimeWorkRequestBuilder<FirstWorker>()
+            .setInputData(inputData)
+            .build()
+
+        // Worker kedua
+        val secondWork = OneTimeWorkRequestBuilder<SecondWorker>().build()
+
+        // Jalankan secara berurutan
+        WorkManager.getInstance(this)
+            .beginWith(firstWork)
+            .then(secondWork)
+            .enqueue()
+
+        Toast.makeText(this, "WorkManager started!", Toast.LENGTH_SHORT).show()
     }
 }
